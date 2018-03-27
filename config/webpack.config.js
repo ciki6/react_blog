4fd8b710cf00b648407a0ed5cc1daaac1eee8838
ciki6 <18694942447@163.com>
2018-03-27 18:52:11 +0800

    update webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -52,5 +52,5 @@ module.exports = {
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
         new webpack.HotModuleReplacementPlugin()//热加载插件
-    ],
+    ]
 }
